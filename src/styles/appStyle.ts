e94144649901b080cc57d7504784713407467ff3
Your Name <you@example.com>
2021-02-28 19:41:58 -0300

    Adicionando responsividade à Home

--- src/styles/appStyle.ts
+++ src/styles/appStyle.ts
@@ -10,6 +10,7 @@ export const GlobalStyles = createGlobalStyle`
         margin: 0%;
         box-sizing: border-box;
         font-family: 'Orbitron', sans-serif;
+        overflow: visible;
     }
 
 
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -6,7 +6,7 @@ export const Container = styled.div`
     margin: 0;
     background-image: linear-gradient(to bottom, var(--blue), var(--blue) );
     background-repeat: no-repeat;
-    overflow: hidden;
+    overflow: visible ;
 `
 
 export const Header = styled.div`
@@ -35,6 +35,18 @@ export const Header = styled.div`
     > svg { 
         color: var(--white);
         font-size: 1.3rem; 
+    }
+
+    @media(min-width: 768px){ 
+        height: 70px;
+        
+        > p { 
+            font-size: 1.8rem;
+        }
+
+        >svg{ 
+            font-size: 1.8rem;
+        }
     }
 `
 
@@ -73,7 +85,7 @@ export const Content = styled.main`
         color: var(--white);
 
         padding: 3% 10px;
-        margin: 15% 0 0;
+        margin: 5% 0 0;
 
         border: 3px solid var(--golden);
         border-radius: 7px;
@@ -84,6 +96,7 @@ export const Content = styled.main`
 
     > img {
         margin-top: 10vh;
+        pointer-events: none;
     }
 
     &:after{ 
@@ -95,5 +108,65 @@ export const Content = styled.main`
         height: 7px;
         border: 1px solid var(--white);
         background-color: var(--white)
+    }
+
+
+    @media(min-width: 768px){
+        height: 45vh;
+        /* flex-direction: col; */
+        align-items:center;
+
+        >h2, h1 {
+            margin: 2.5% 0 ;
+        }
+
+        >button {
+            margin: 0;
+            font-size: 1.8rem;
+        }
+
+        >img{
+            position: absolute;
+            bottom: 0;
+            margin-top: 0;
+        }
+
+        &:after{
+            height: 10px;
+         }
+    
+    }
+
+    @media(min-width: 1024px) {
+        height: 90vh;
+
+        >h2 {
+            font-size: 2.5rem;
+        }
+
+        > h1 {
+            font-size: 2.8rem
+        }
+
+        > button { 
+            border: 0;
+
+            padding: 5px;
+            cursor: pointer;
+            background-image: linear-gradient(to bottom, var(--golden), rgba(0,0,0,.0));
+            background-repeat: no-repeat;
+
+            background-position: 0 -100px;
+            transition: background-position .5s, color .5s, filter .5s;
+            filter: brightness(80%);
+        
+            &:hover{ 
+                background-position: 0 0;
+                color: var(--blue-text);
+                filter: brightness(100%);
+
+            }
+            
+        }
     }
 `
